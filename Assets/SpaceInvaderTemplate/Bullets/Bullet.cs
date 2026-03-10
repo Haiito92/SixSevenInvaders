cf00f830a7a2a331e9cefb19585561651cfa8749
jeremy-lombard <jeremylombard.mail@gmail.com>
2026-03-10 11:59:09 +0100

    [FIX] : Bullet Spawn Timer

--- Assets/SpaceInvaderTemplate/Bullets/Bullet.cs
+++ Assets/SpaceInvaderTemplate/Bullets/Bullet.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] Vector3 startVelocity; 
+    [SerializeField] Vector3 startVelocity;
+    [SerializeField] private float timeBeforeDeath = 15.0f;
 
     // Start is called before the first frame update
     void Awake()
     {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.linearVelocity = startVelocity;
+        StartCoroutine(DeathTimer(timeBeforeDeath));
+    }
+
+    private IEnumerator DeathTimer(float lifeTime)
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
     }
 }
