de7efe2798f357be2b63989225300d55fa8ba653
Haiito92 <hannaantoinebusiness@gmail.com>
2026-03-10 09:42:50 +0100

    [GP][ADD] ScoreManager (Add/Remove score) and Enemy score on death

--- Assets/SpaceInvaderTemplate/Invaders/Invader.cs
+++ Assets/SpaceInvaderTemplate/Invaders/Invader.cs
@@ -2,16 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Invader : MonoBehaviour
 {
     [SerializeField] private Bullet bulletPrefab = null;
     [SerializeField] private Transform shootAt = null;
     [SerializeField] private string collideWithTag = "Player";
-
+    [SerializeField] private UInt64 m_scoreOnDeath = 100;
+    
     internal Action<Invader> onDestroy;
+    [SerializeField] internal UnityEvent m_onDestroyUnityEvent;
 
     public Vector2Int GridIndex { get; private set; }
+
+    private void Awake()
+    {
+        m_onDestroyUnityEvent.AddListener(() => { onDestroy.Invoke(this); });
+    }
 
     public void Initialize(Vector2Int gridIndex)
     {
@@ -20,7 +28,8 @@ public class Invader : MonoBehaviour
 
     public void OnDestroy()
     {
-        onDestroy?.Invoke(this);
+        m_onDestroyUnityEvent.Invoke();
+        ScoreManager.Instance?.AddScore(m_scoreOnDeath);
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
--- Assets/_Project/Scripts/Score/ScoreManager.cs
+++ Assets/_Project/Scripts/Score/ScoreManager.cs
@@ -1,16 +1,44 @@
+using System;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private UInt64 m_score;
+    
+    #region Singleton
+    private static ScoreManager m_instance;
+    public static ScoreManager Instance => m_instance;
+
+    private void InitSingleton()
     {
-        
+        if (m_instance != null && m_instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            m_instance = this;
+        }
+    }
+    #endregion
+    
+    private void Awake()
+    {
+        InitSingleton();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddScore(UInt64 scoreToAdd)
     {
-        
+        m_score = InvadersMathUtils.Min(m_score + scoreToAdd, UInt64.MaxValue);
+    }
+
+    public void RemoveScore(UInt64 scoreToRemove)
+    {
+        m_score = InvadersMathUtils.Max(m_score - scoreToRemove, UInt64.MinValue);
+    }
+
+    public void SaveScore()
+    {
+        //TODO
     }
 }
