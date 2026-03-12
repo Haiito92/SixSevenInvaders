1a47e43149bca0b7238762677d1318c64ee320d8
Haiito92 <hannaantoinebusiness@gmail.com>
2026-03-12 14:47:30 +0100

    [GP][ADD] Toggle funcs for VfxDebug

--- Assets/_Project/Scripts/Debug/VfxDebug.cs
+++ Assets/_Project/Scripts/Debug/VfxDebug.cs
@@ -7,6 +7,37 @@ public static class VfxDebug
     public static bool BlockPlayerEffects = false;
     public static bool BlockEnemiesEffects = false;
 
+    public static void ToggleRipple()
+    {
+        BlockRipple = !BlockRipple;
+    }
+    
+    public static void ToggleFullScreenPixel()
+    {
+        BlockFullScreenPixel  = !BlockFullScreenPixel;
+    }
+    
+    public static void TogglePlayerEffects()
+    {
+        BlockPlayerEffects  = !BlockPlayerEffects;
+    }
+    
+    public static void ToggleEnemiesEffects()
+    {
+        BlockEnemiesEffects  = !BlockEnemiesEffects;
+    }
+    
+    public static void ToggleAllEffects()
+    {
+        if (BlockRipple || BlockFullScreenPixel || BlockPlayerEffects || BlockEnemiesEffects)
+        {
+            UnblockAllEffects();
+            return;
+        }
+        
+        BlockAllEffects();
+    }
+    
     public static void BlockAllEffects()
     {
         BlockRipple = true;
